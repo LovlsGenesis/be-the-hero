73ff8e7cac0d9fa367b57058ae59e0b6e7a2c631
Guillaume Zeldine <g.zeldine@hotmail.fr>
2020-03-28 21:45:15 -0300

    Half of it

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+
+import './style.css'
+
+import heroesImg from '../../assets/heroes.png'
+import logoImg from '../../assets/logo.svg'
+
+export default function Logon() {
+    return (
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero"/>
+
+                <form>
+                    <h1>Faça seu logo</h1>
+                    <input placeholder="Sua ID"/>
+                    <button className="button" type="submit">Entrar</button>
+                    <a href="/register">
+                        <FiLogIn size={16} color="#E02041"/>
+                        Nao tenho cadastro
+                    </a>
+                </form>
+            </section>
+            <img src={heroesImg} alt="Heroes"/>
+        </div>
+    );
+}
